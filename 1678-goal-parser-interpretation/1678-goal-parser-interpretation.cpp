0241883233ee6f0a1c687931cc9a7db54af4d704
richestc <83532927+richestc@users.noreply.github.com>
2023-09-16 00:34:28 +0930

    Time: 0 ms (100.00%), Space: 6.7 MB (5.23%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string interpret(string command) {
+        
+        string res = "";
+        
+        for(int i=0;i<command.size();i++){
+            
+            if( command[i] == 'G' ){
+                res = res + "G";
+            }
+            else if( command[i] == '(' ){
+                if( command[i+1] == ')' ){
+                    res = res + "o";
+                }
+                if( command [i+1] == 'a' ){
+                    res = res + "al";
+                }
+            }
+            
+        }
+        
+        return res;
+        
+        
+    }
+};
