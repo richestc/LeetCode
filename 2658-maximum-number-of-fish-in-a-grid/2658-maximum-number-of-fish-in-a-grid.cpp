15866ccbc0049568e59ec10d194a370a2f497814
richestc <83532927+richestc@users.noreply.github.com>
2023-10-22 18:38:34 +1030

    Time: 53 ms (70.79%), Space: 88.9 MB (78.20%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void solve(int i,int j,int &a,int &b,int &sum,int &ans,vector<vector<int>> &g){
+    void solve(int i,int j,int &sum,int &ans,vector<vector<int>> &g){
         ans = max(ans,sum);
-        if(i<0||j<0||i>=a||j>=b||g[i][j]==0){
+        if(i<0||j<0||i>=g.size()||j>=g[0].size()||g[i][j]==0){
             return;
         }
         sum += g[i][j];
         g[i][j] = 0;
-        solve(i+1,j,a,b,sum,ans,g);
-        solve(i,j+1,a,b,sum,ans,g);
-        solve(i-1,j,a,b,sum,ans,g);
-        solve(i,j-1,a,b,sum,ans,g);
+        solve(i+1,j,sum,ans,g);
+        solve(i,j+1,sum,ans,g);
+        solve(i-1,j,sum,ans,g);
+        solve(i,j-1,sum,ans,g);
     }
     int findMaxFish(vector<vector<int>>& grid) {
         int ans = 0;
-        int i,j,a = grid.size(),b = grid[0].size();
         int sum = 0;
-        for(i = 0; i < a; i++){
-            for(j = 0; j < b; j++){
+        
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j]){
                     sum = 0;
-                    solve(i,j,a,b,sum,ans,grid);
+                    solve(i,j,sum,ans,grid);
                 }
             }
         }
