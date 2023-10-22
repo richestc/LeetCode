ebd4f90323375135afb3c94047e6a631482f3b5a
richestc <83532927+richestc@users.noreply.github.com>
2023-10-22 18:39:14 +1030

    Time: 47 ms (86.97%), Space: 88.8 MB (84.27%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
+    //AMPERSANDS ARE IMPORTANT!!! ???
     void solve(int i,int j,int &sum,int &ans,vector<vector<int>> &g){
         ans = max(ans,sum);
         if(i<0||j<0||i>=g.size()||j>=g[0].size()||g[i][j]==0){
             return;
         }
+        
         sum += g[i][j];
         g[i][j] = 0;
+        
         solve(i+1,j,sum,ans,g);
         solve(i,j+1,sum,ans,g);
         solve(i-1,j,sum,ans,g);
